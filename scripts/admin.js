4edf2c09d109f7fa71556b6626f159e13aceb9a8
ananyaas4 <ananyaasgowda@gmail.com>
2024-12-28 15:53:28 +0530

    Created Admin Page

---
+++ scripts/admin.js
@@ -0,0 +1,123 @@
+import {baseUrl} from "./baseUrl.js";
+
+const loginData = JSON.parse(localStorage.getItem("loginData"));
+if(!loginData || loginData.email !== 'admin@empher.com')
+{
+    alert("Admin Not Logged In.");
+    window.location.href = "index.html";
+}
+
+window.onload = () =>{
+    getData();
+}
+
+let form = document.getElementById("form");
+form.addEventListener("submit", function(){
+    event.preventDefault();
+    let title = form.title.value;
+    let author = form.author.value;
+    let category = form.category.value;
+    let bookobj = {title, author, category, isAvailable:false, borrowedDays: null,isVerified: false};
+    fetch(`${baseUrl}/books`,{
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(bookobj)
+    })
+    .then(()=>{
+        alert("Book Added Successfully!");
+        form.reset();
+        getData();
+    })
+    .catch((err)=>{
+        alert("Something went wrong");
+        console.log(err);
+    })
+});
+
+function getData(){
+    fetch(`${baseUrl}/books`)
+    .then((res)=>res.json())
+    .then((data)=>displayBooks(data))
+    .catch((err)=>{
+        alert("Something went wrong");
+        console.log(err);
+    })
+}
+
+function displayBooks(arr){
+    let cont = document.getElementById("cont");
+    cont.innerHTML = "";
+    arr.map((ele,i)=>{
+        let card = document.createElement("div");
+
+        let title = document.createElement("h4");
+        title.textContent = `${ele.title}`;
+
+        let author = document.createElement("p");
+        author.textContent = `Author: ${ele.author}`;
+
+        let category = document.createElement("p");
+        category.textContent = `Category: ${ele.category}`;
+
+        let availabilityStatus = document.createElement("p");
+        availabilityStatus.textContent = `Availability Status: ${ele.isAvailable}`;
+
+        let borrowedDays = document.createElement("p");
+        borrowedDays.textContent = `Borrowed Days: ${ele.borrowedDays}`;
+
+        let verifyBtn = document.createElement("button");
+        verifyBtn.textContent = "Verify Book";
+        verifyBtn.addEventListener("click",function(){
+            if(confirm("Are you sure to Verify?"))
+            {
+                verifyFnc(ele);
+            }
+        });
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete Book";
+        deleteBtn.addEventListener("click",function(){
+            if(confirm("Are you sure to Delete..??"))
+            {
+                deleteFnc(ele);
+            }
+        })
+        card.append(title, author, category, availabilityStatus, borrowedDays, verifyBtn, deleteBtn);
+        cont.append(card);
+    });
+}
+
+function verifyFnc(ele){
+    let updatedBookObj = {...ele, isVerified: true};
+    fetch(`${baseUrl}/books/${ele.id}`,{
+        method: "PATCH",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(updatedBookObj)
+    })
+    .then(()=>{
+        alert("Book Verified Successfully!");
+        getData();
+    })
+    .catch((err)=>{
+        alert("Something went wrong");
+        console.log(err);
+    })
+}
+
+function deleteFnc(ele){
+    fetch(`${baseUrl}/books/${ele.id}`,{
+        method: "DELETE",
+    })
+    .then(()=>{
+        alert("Book Deleted Successfully!");
+        getData();
+    })
+    .catch((err)=>{
+        alert("Something went wrong");
+        console.log(err);
+    })
+}
--- scripts/login.js
+++ scripts/login.js
@@ -3,18 +3,18 @@ form.addEventListener("submit", function(){
     event.preventDefault();
     let email = form.email.value;
     let password = form.password.value;
-    let userobj = {email,password};
+    let userObj = {email,password};
     if(email=="admin@empher.com" && password=="empher@123")
     {
         alert("Logged in as Admin.");
         window.location.href = "admin.html";
-        localStorage.setItem("loginData",JSON.stringify(userobj));
+        localStorage.setItem("loginData",JSON.stringify(userObj));
     }
     else if(email=="user@empher.com" && password=="user@123")
     {
         alert("Logged in as User.");
         window.location.href = "books.html";
-        localStorage.setItem("loginData",JSON.stringify(userobj));
+        localStorage.setItem("loginData",JSON.stringify(userObj));
     }
     else{
         alert("Invalid Email or Password");
