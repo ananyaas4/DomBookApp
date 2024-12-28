1916877ac9a3632b6dcde4ee7f906214642c2330
ananyaas4 <ananyaasgowda@gmail.com>
2024-12-28 14:47:40 +0530

    Updated Login Page

--- scripts/login.js
+++ scripts/login.js
@@ -3,15 +3,18 @@ form.addEventListener("submit", function(){
     event.preventDefault();
     let email = form.email.value;
     let password = form.password.value;
+    let userobj = {email,password};
     if(email=="admin@empher.com" && password=="empher@123")
     {
         alert("Logged in as Admin.");
         window.location.href = "admin.html";
+        localStorage.setItem("loginData",JSON.stringify(userobj));
     }
     else if(email=="user@empher.com" && password=="user@123")
     {
         alert("Logged in as User.");
         window.location.href = "books.html";
+        localStorage.setItem("loginData",JSON.stringify(userobj));
     }
     else{
         alert("Invalid Email or Password");
